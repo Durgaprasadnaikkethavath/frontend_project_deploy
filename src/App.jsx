f9597a90554e7e9835b000ed4ec9076348c4641c
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-20 18:54:25 +0530

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "./Context/ShopContext";
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
       <p>API Working</p>
+      <BrowserRouter>
+        <ShopContextProvider>
+          <Navbar />
+        </ShopContextProvider>
+      </BrowserRouter>
     </div>
   );
 };
