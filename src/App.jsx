60b61dd4129bec4eb8fc4fbba6cc50151de75d4b
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-20 19:03:06 +0530

    error handle

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./Context/ShopContext";
-import ImageCarousel from "./Components/ImageCarousel";
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
       <p>API Working</p>
+      <h1>Carousel Images are working at this time</h1>
       <BrowserRouter>
         <ShopContextProvider>
           <Navbar />
-          <ImageCarousel />
         </ShopContextProvider>
       </BrowserRouter>
     </div>
