0decb153bb25306dca484a36c94e113bdcc31a0e
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-20 20:58:25 +0530

    total

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ShopContextProvider from "./Context/ShopContext";
-import Hero from "./Components/Hero";
-import LatestCollection from "./Components/LatestCollection";
+import Home from "./Pages/Home";
+import { ToastContainer } from "react-toastify";
+import Collection from "./Pages/Collection";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import Login from "./Pages/Login";
+import PlaceOrder from "./Pages/PlaceOrder";
+import Order from "./Pages/Order";
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
-      <p>API Working</p>
-      <h1>Carousel Images are working at this time</h1>
       <BrowserRouter>
         <ShopContextProvider>
+          <ToastContainer />
           <Navbar />
-          <Hero />
-          <LatestCollection />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/collection" element={<Collection />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/place-order" element={<PlaceOrder />} />
+            <Route path="/order" element={<Order />} />
+          </Routes>
         </ShopContextProvider>
       </BrowserRouter>
     </div>
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Hero from "../Components/Hero";
+import LatestCollection from "../Components/LatestCollection";
+import BestSeller from "../Components/BestSeller";
+import OurPolicy from "../Components/OurPolicy";
+import Email from "../Components/Email";
+import Footer from "../Components/Footer";
+// import ImageCarousel from "../Components/ImageCarousel";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <LatestCollection />
+      <BestSeller />
+      <OurPolicy />
+      <Email />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
